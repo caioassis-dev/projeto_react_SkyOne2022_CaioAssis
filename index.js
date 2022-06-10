6820cc1f32754604d0d4659b38d825184e7249fd
caioassis-dev <caiofreire_1@hotmail.com>
2022-06-10 16:45:36 -0300

    atualizando render para determinar e exibir status do jogo

--- index.js
+++ index.js
@@ -11,14 +11,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
-
   handleclick(i) {
     const squares = this.state.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
@@ -34,8 +26,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     )
   }
@@ -66,7 +58,27 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      xIsNext: true
+    }
+  }
   render() {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const winner = calculateWinner(current.squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
     return (
       <div className="game">
         <div className="game-board">
