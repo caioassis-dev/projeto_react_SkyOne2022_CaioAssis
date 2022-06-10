ecec8df7333df0e6cf9d956e920ecf39b1b50abc
caioassis-dev <caiofreire_1@hotmail.com>
2022-06-10 17:08:58 -0300

    alteração no método handleclick e o render do componente Game

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function Square(props) {
 
 class Board extends React.Component {
   handleclick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
@@ -25,6 +25,7 @@ class Board extends React.Component {
           squares: squares
         }
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     })
   }
@@ -83,7 +84,7 @@ class Game extends React.Component {
   }
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares)
 
     const moves = history.map((step, move) => {
