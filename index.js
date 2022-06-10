11ff60e7db58ee2a9264f75e29eebaf6c1f5391b
caioassis-dev <caiofreire_1@hotmail.com>
2022-06-10 11:42:53 -0300

    adicionando import ao arquivo JS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
 class Square extends React.Component {
   render() {
@@ -44,18 +45,14 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-        <div>{/*status*/}</div>
-        <ol>{/* TODO */}</ol>
+          <div>{/*status*/}</div>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
-    );
+    )
   }
 }
 
 // ================================
 
-ReactDOM.render(
-  <Game/>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Game />, document.getElementById('root'))
